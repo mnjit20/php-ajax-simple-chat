ac4064fe3df87598e815cf40bcd4ab41c842beb6
Manjeet Verma <mnjit.vrma@gmail.com>
2024-05-21 13:40:40 +0200

    removed all files

--- simple_chat/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/**********************************************\
-* Copyright (c) 2013 Manolis Agkopian          *
-* See the file LICENCE for copying permission. *
-\**********************************************/
-
-header('Location: ../');
-?>
